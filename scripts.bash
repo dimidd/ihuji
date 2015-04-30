83b244aae500b6465d8b1fe1426cbb5e08bd215c
Dimid Duchovny <dimidd@gmail.com>
2015-04-30 18:41:00 +0300

    tmp
    Signed-off-by: Dimid Duchovny <dimidd@gmail.com>

--- scripts.bash
+++ scripts.bash
@@ -1,2 +1,3 @@
 #!/bin/bash
 while read l ; do fields=$(echo $line | tr -dc ',' | wc -c); ((fields <= 9)) && echo $l; done < /tmp/courses.csv > cl_courses.csv
+cat /tmp/groups.csv | cut -d',' -f 1-3 > cl_groups.csv
--- web2py/parameters_8123.py
+++ web2py/parameters_8123.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$9cb7fe7df025952b$e96034df9a427a723e6f2c5a92e5bbb0ec06977f"
+password="pbkdf2(1000,20,sha512)$af47e455be9f90ef$ee3c666dd085ea5410cab9112a4b8756d41c7c21"
